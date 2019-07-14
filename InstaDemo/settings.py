ec85fa1ffbe3ef8c3f540e03a7bce2e15adeb4c2
Frank Zhou <hzhou1125@gmail.com>
2019-07-14 12:37:44 -0700

    Authentication

--- InstaDemo/settings.py
+++ InstaDemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hf@(59!7dtzfp8804yj@r%t$lr98)+(9y2*gz9u_*1!g+0@gd9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
+
+AUTH_USER_MODEL = 'Insta.InstaUser'
